b571a9124bfcb04c09c2ee8ab2d22ae61f158717
Schwuchtelini <schwuchtelini@gmail.com>
2015-02-04 03:28:43 +0100

    benis

--- code.js
+++ code.js
@@ -153,8 +153,9 @@ g.text = 'cust0m = {};\r\n' +
 '              }\r\n' +
 '              lastRow.append(this.prepareThumbsForInsertion(html));\r\n' +
 '          }\r\n' +
-'          if(items.length - fill > 0) {var html = this.buildItemRows(items, fill, items.length, position);\r\n' +
-'          this.$streamContainer.append(this.prepareThumbsForInsertion(html));}\r\n' +
+'          var html = ""; \r\n' +
+'          if(items.length - fill > 0) html = this.buildItemRows(items, fill, items.length, position);\r\n' +
+'          this.$streamContainer.append(this.prepareThumbsForInsertion(html));\r\n' +
 '      }\r\n' +
 '      if (this.jumpToItem) {\r\n' +
 '          var target = $("#item-" + this.jumpToItem);\r\n' +
