565ad956d7fce5e8941c6a0dadd9da94d5a625f3
Schwuchtelini <schwuchtelini@gmail.com>
2015-02-06 20:46:00 +0100

    benis

--- code.js
+++ code.js
@@ -271,7 +271,7 @@ g.text = 'cust0m = {};\r\n' +
 '        debugger;\r\n' +
 '        if(cust0m.bullshit && (location.indexOf("new") != 0 || (cust0m.disableLoad && location == "new"))) cust0m.disable_bullshit();\r\n' +
 '        if(cust0m.best_of && (location.indexOf("top") != 0 || (cust0m.disableLoad && location == "top"))) cust0m.disable_best_of();\r\n' +
-'        if(location == documemnt.href)\r\n' +
+'        if(location == document.href)\r\n' +
 '        {\r\n' +
 '            p.navigateToOrginal(location == "top" ? "new" : "top", mode);\r\n' +
 '            setTimeout(function() {p.navigateToOrginal(location, mode);}, 500);\r\n' +
