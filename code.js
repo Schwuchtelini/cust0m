9ebd3b0023f0e096706c01c1846059c21b1067af
Schwuchtelini <schwuchtelini@gmail.com>
2015-02-06 01:41:28 +0100

    best of

--- code.js
+++ code.js
@@ -44,7 +44,7 @@ $(".user-info.user-only").prepend('<div class="cust0m_settings"> \r\n' +
 '    </div> \r\n' +
 '    <div class="cust0m_middle"> \r\n' +
 '        <div class="cust0m_label cust0m_lable_1">Best of:</div> \r\n' +
-'        <div id="cust0m_input_bets_of" class="cust0m_triggers"> \r\n' +
+'        <div id="cust0m_input_best_of" class="cust0m_triggers"> \r\n' +
 '             <div class="cust0m_trigger cust0m_trigger_on active" onclick="$(this).parent().children().removeClass(\'active\'); $(this).addClass(\'active\');">ON</div> \r\n' +
 '             <div class="cust0m_trigger cust0m_trigger_off" onclick="$(this).parent().children().removeClass(\'active\'); $(this).addClass(\'active\'); ">OFF</div> \r\n' +
 '        </div> \r\n' +
