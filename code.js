48d3d4732f7553cbc4a2a65dac5902f2e0d1b3d4
Schwuchtelini <schwuchtelini@gmail.com>
2015-02-07 01:13:30 +0100

    loading weg

--- code.js
+++ code.js
@@ -156,12 +156,12 @@ g.text = 'cust0m = {};\r\n' +
 '           data2.items = data.items;\r\n' +
 '           data = data2;\r\n' +
 '           console.log("cust0m Pr0gramm: Lade Items. Bis jetzt: " + data.items.length + " mit " + options);\r\n' +
-'           if((cust0m.bullshit || cust0m.best_of) && round > 50) $(".spinner").text((((data.items.length/120) * 100 + "").substr(0, 3) + "%"));\r\n' +
+'           //if((cust0m.bullshit || cust0m.best_of) && round > 50) $(".spinner").text((((data.items.length/120) * 100 + "").substr(0, 3) + "%"));\r\n' +
 '           if((cust0m.bullshit || cust0m.best_of) && data.items.length < 120 && position == p.Stream.POSITION.PREPEND && !stream.reached.start) { console.log("cust0m Pr0gramm: Lade anfang"); stream._loadCust0m({newer: stream._newestId}, callback, data, round); }\r\n' +
 '           else if((cust0m.bullshit || cust0m.best_of) && data.items.length < 120 && !stream.reached.end) { console.log("cust0m Pr0gramm: Lade end"); stream._loadCust0m({older: stream._oldestId}, callback, data, round); }\r\n' +
 '           else \r\n' +
 '           {\r\n' +
-'               if((cust0m.bullshit || cust0m.best_of) && round > 50) $(".spinner").html("<div class=\'bounce1\'></div><div class=\'bounce2\'></div><div class=\'bounce3\'></div>");\r\n' +
+'               //if((cust0m.bullshit || cust0m.best_of) && round > 50) $(".spinner").html("<div class=\'bounce1\'></div><div class=\'bounce2\'></div><div class=\'bounce3\'></div>");\r\n' +
 '               stream._oldestId = _oldestId;\r\n' +
 '               stream._newestId = _newestId;\r\n' +
 '               position = stream._processResponse(data);\r\n' +
