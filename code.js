d6f01f553defa3fed1e7dd915d117c2a88c7a4d5
Schwuchtelini <schwuchtelini@gmail.com>
2015-02-08 18:00:59 +0100

    pfeile

--- code.js
+++ code.js
@@ -51,7 +51,7 @@ $(".user-info.user-only").prepend('<div class="cust0m_settings"><div class="cust
 '    </div> \r\n' +
 '    <div class="cust0m_middle"> \r\n' +
 '        <div class="cust0m_help">Willst du die alten Pfeile wieder haben?</div> \r\n' +
-'        <div class="cust0m_label cust0m_lable_1">Alte Pfeile anzeigen:</div> \r\n' +
+'        <div class="cust0m_label cust0m_lable_1">Die alte Pfeile anzeigen:</div> \r\n' +
 '        <div id="cust0m_input_pfeil" class="cust0m_triggers"> \r\n' +
 '             <div class="cust0m_trigger cust0m_trigger_on active" onclick="$(this).parent().children().removeClass(\'active\'); $(this).addClass(\'active\');">ON</div> \r\n' +
 '             <div class="cust0m_trigger cust0m_trigger_off" onclick="$(this).parent().children().removeClass(\'active\'); $(this).addClass(\'active\'); ">OFF</div> \r\n' +
