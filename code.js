8be71551ff893cddf5658b8eea4f4e40c348f778
Schwuchtelini <schwuchtelini@gmail.com>
2015-02-08 21:28:50 +0100

    fehler test

--- code.js
+++ code.js
@@ -681,7 +681,7 @@ function update_settings()
                  changeCss('.cust0m_best_of_middle', 'display: none');
             });
         }
-        setTimeout(function() { if($('.main-message').text().indexOf('Das Bild wurde als') == -1) $(window).resize();}, 1000);
+        setTimeout(function() { if($('.main-message').text().indexOf('Das Bild wurde als') == -1) $(window).resize();}, 5000);
     });
 }
 
