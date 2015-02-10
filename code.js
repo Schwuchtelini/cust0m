56fb4b9e751851f1ba350c77cb33288fd66b1cee
Schwuchtelini <schwuchtelini@gmail.com>
2015-02-10 16:45:43 +0100

    klappen

--- code.js
+++ code.js
@@ -115,7 +115,7 @@ $(".user-info.user-only").prepend('<div class="cust0m_settings"><div class="cust
 '    <div class="cust0m_middle cust0m_posts"> \r\n' +
 '        <div class="cust0m_help">Gibt dir die Möglichkeit Kommentare ein- und auszuklappen.</div> \r\n' +
 '        <div class="cust0m_label cust0m_lable_1">Ein- und ausklappbare Kommentare:</div> \r\n' +
-'        <div id="cust0m_input_kommentarlinienbreite" class="cust0m_triggers"> \r\n' +
+'        <div id="cust0m_input_kommentarklappen" class="cust0m_triggers"> \r\n' +
 '             <div class="cust0m_trigger cust0m_trigger_on active" onclick="$(this).parent().children().removeClass(\'active\'); $(this).addClass(\'active\');">ON</div> \r\n' +
 '             <div class="cust0m_trigger cust0m_trigger_off" onclick="$(this).parent().children().removeClass(\'active\'); $(this).addClass(\'active\'); ">OFF</div> \r\n' +
 '        </div> \r\n' +
