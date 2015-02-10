404495d2c1e44c35d6f7fdf674db5896c1e693e7
Schwuchtelini <schwuchtelini@gmail.com>
2015-02-10 16:00:32 +0100

    fehler

--- code.js
+++ code.js
@@ -79,7 +79,7 @@ $(".user-info.user-only").prepend('<div class="cust0m_settings"><div class="cust
 '             <div class="cust0m_trigger cust0m_trigger_on active" onclick="$(this).parent().children().removeClass(\'active\'); $(this).addClass(\'active\');">ON</div> \r\n' +
 '             <div class="cust0m_trigger cust0m_trigger_off" onclick="$(this).parent().children().removeClass(\'active\'); $(this).addClass(\'active\'); ">OFF</div> \r\n' +
 '        </div> \r\n' +
-'    </div> \r\n' ++
+'    </div> \r\n' +
 '    <div class="cust0m_middle cust0m_posts"> \r\n' +
 '        <div class="cust0m_help">Dadurch werden die Kommentare des OPs ganz oben angezeigt.</div> \r\n' +
 '        <div class="cust0m_label cust0m_lable_1">OP nach oben:</div> \r\n' +
